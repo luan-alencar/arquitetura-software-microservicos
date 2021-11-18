d6f2c6c85f01f25c58b71000772b57d8ed4c3932
luan-alencar <luanalencar134@gmail.com>
2021-11-18 15:23:45 -0300

    Configuracoes iniciais para integracao usuarioservice -> configserver

--- usuarioservice/src/main/java/david/augusto/luan/usuarioservice/AcessoserviceApplication.java
+++ usuarioservice/src/main/java/david/augusto/luan/usuarioservice/AcessoserviceApplication.java
@@ -2,8 +2,10 @@ package david.augusto.luan.usuarioservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
 public class AcessoserviceApplication {
