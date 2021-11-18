5800204a0cdf14589f85b1fc544bb573ef021fc1
luan-alencar <luanalencar134@gmail.com>
2021-11-18 16:41:29 -0300

    adicionando modulo eurekaserver e alteracoes em gateway

--- gateway/src/main/java/david/augusto/luan/gateway/GatewayApplication.java
+++ gateway/src/main/java/david/augusto/luan/gateway/GatewayApplication.java
@@ -2,9 +2,7 @@ package david.augusto.luan.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
 public class GatewayApplication {
 
