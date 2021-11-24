25757151f47be8ec3533626a1febb50fff98af66
luan-alencar <luanalencar134@gmail.com>
2021-11-23 21:54:56 -0300

    Adicionando configuracao no arquivo application.yml - sistema rodando

--- usuarioservice/src/main/java/david/augusto/luan/usuarioservice/config/ApplicationProperties.java
+++ usuarioservice/src/main/java/david/augusto/luan/usuarioservice/config/ApplicationProperties.java
@@ -5,10 +5,10 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ConfigurationProperties(prefix = "application-dev.mail")
 @Getter
 @Setter
+@Configuration
+@ConfigurationProperties(prefix = "application.mail")
 public class ApplicationProperties {
 
     private String enderecoRemetente;
